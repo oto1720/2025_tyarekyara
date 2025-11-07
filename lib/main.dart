5e84cfabea03ab07113df9ff97261ab7267c84a2
oto1720 <kotaro17206@icloud.com>
2025-11-07 15:16:22 +0900

    [add]devicepreviewを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:tyarekyara/core/route/app_router.dart';
 import 'firebase_options.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +16,13 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const ProviderScope(child: MyApp()));
+
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode, // デバッグモードとプロファイルモードで有効
+      builder: (context) => const ProviderScope(child: MyApp()),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,6 +30,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(routerConfig: router);
+    return MaterialApp.router(
+      routerConfig: router,
+      // DevicePreview設定
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      // デバッグバナーを非表示
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
